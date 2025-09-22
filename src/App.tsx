2cf60cb69050d3bf8a1d4d9dc456b678ab5bca79
xlsama <im.xlsama@gmail.com>
2025-09-22 15:23:58 +0800

    Refactor App component: Update color toggle logic and improve layout structure. Adjust text and input elements for better user interaction.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 
 export function App(props: { onRender?: () => void }) {
   const [color, $color] = useState('red')
+  const [inputValue, $inputValue] = useState('')
 
   useEffect(() => {
     console.info('Hello, ReactLynx')
@@ -13,14 +14,24 @@ export function App(props: { onRender?: () => void }) {
   const onTap = () => $color(color === 'red' ? 'blue' : 'red')
 
   return (
-    <view className="h-screen flex items-center justify-center">
+    <view className="flex flex-col items-center justify-center h-screen gap-5 px-5">
       <text
         bindtap={onTap}
         className="text-5xl active:scale-95 transition-all"
         style={{ color }}
       >
-        Hello, ReactLynx
+        Hello
       </text>
+
+      <input
+        className="border border-gray-300 rounded-3xl p-4 w-full text-[18px]"
+        placeholder="Input Something"
+        bindinput={(res) => {
+          $inputValue(res.detail.value)
+        }}
+      />
+
+      <text className="text-xl">You Input: {inputValue}</text>
     </view>
   )
 }
